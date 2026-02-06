2301f3d09719e9c1e9143940e2819b179889e277
fox0 <15684995+fox0@users.noreply.github.com>
2026-02-06 22:52:18 +0700

    patch tor-chanmgr

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use anyhow::Result;
-use tor_chanmgr::{ChanMgr, ChannelConfig, ChannelUsage, Dormancy};
+use tor_chanmgr::{ChanMgr, ChanMgrConfig, ChannelUsage, Dormancy};
 // use tor_chanmgr::factory::ChannelFactory;
+use tor_proto::channel::ChannelBuilder;
 use tor_linkspec::OwnedChanTarget;
-use tor_netdir::params::NetParameters;
-use tor_memquota::mtracker::MemoryQuotaTracker;
 use tor_memquota::Config as MemquotaConfig;
+use tor_memquota::mtracker::MemoryQuotaTracker;
+use tor_netdir::params::NetParameters;
 use tor_rtcompat::PreferredRuntime;
 
 #[tokio::main]
@@ -13,23 +14,19 @@ async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
     let runtime = PreferredRuntime::current().unwrap();
-    let config = ChannelConfig::default();
+    let config = ChanMgrConfig::default();
     let dormancy = Dormancy::default();
     let netparams = NetParameters::default();
 
     let memquota_config = MemquotaConfig::builder().build().unwrap();
     let memquota = MemoryQuotaTracker::new(&runtime, memquota_config).unwrap();
 
-    let keymgr = None;  // TODO
-
-    let chanmgr = ChanMgr::new(runtime, &config, dormancy, &netparams, memquota, keymgr);
+    let chanmgr = ChanMgr::new(runtime, config, dormancy, &netparams, memquota);
 
     // let target = OwnedChanTarget::from_chan_target(target);
     // let usage = ChannelUsage::Dir;
 
     // let t = chanmgr.get_or_launch(&target, usage).await?;
-
-
 
     // let cb = ChannelBuilder::new();
 
